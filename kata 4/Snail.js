a768515bc1f7a648ac42a8da849b5a0f989e0fa4
Seaming <347552878@qq.com>
2024-02-21 23:34:02 +0800

    Update Snail.js

--- kata 4/Snail.js
+++ kata 4/Snail.js
@@ -1,5 +1,23 @@
-/**
- * @param {Array} array
- * @return {Array}
- */
-snail = function (array) {}
+function snail(array) {
+  let result = []
+  while (array.length) {
+    // Add the first row
+    result = result.concat(array.shift())
+
+    // Add the last element of each row
+    for (let i = 0; i < array.length; i++) {
+      result.push(array[i].pop())
+    }
+
+    // Add the last row in reverse order
+    if (array.length) {
+      result = result.concat(array.pop().reverse())
+    }
+
+    // Add the first element of each row in reverse order
+    for (let i = array.length - 1; i >= 0; i--) {
+      result.push(array[i].shift())
+    }
+  }
+  return result
+}
