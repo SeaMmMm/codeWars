cd00321b33dca3019bba3839ee60f3d1db36848d
Seaming <347552878@qq.com>
2024-02-24 08:14:50 +0800

    Create The Hashtag Generator.js

---
+++ kata 5/The Hashtag Generator.js
@@ -0,0 +1,19 @@
+/**
+ * @description: The Hashtag Generator
+ * @param {String} str
+ * @return {String | Boolean}
+ */
+function generateHashtag(str) {
+  if (!str.trim().length) return false
+
+  let result = ['#']
+  const arr = str.split(' ')
+  for (const char of arr) {
+    if (char === '') continue
+
+    result.push(char[0].toUpperCase() + char.slice(1))
+  }
+  result = result.join('')
+
+  return result.length <= 140 ? result : false
+}
